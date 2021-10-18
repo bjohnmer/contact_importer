ff284f9bf5ac625a1a67a47e845a50b30e429137
Johnmer Bencomo <bjohnmer@gmail.com>
2021-10-17 22:52:14 -0500

    fix root route

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 Rails.application.routes.draw do
   default_url_options :host => ENV['RAILS_DEFAULT_URL']
 
+  root to: "contacts#index"
   resources :contacts, only: [:index]
   namespace :imported_files do
    get 'index'
@@ -11,5 +12,4 @@ Rails.application.routes.draw do
   get 'imported_files/:id', to: 'imported_files#show', as: :imported_files_show
 
   devise_for :users, path: 'auth', path_names: { sign_in: 'login', sign_out: 'logout' }
-  root to: "home#index"
 end
